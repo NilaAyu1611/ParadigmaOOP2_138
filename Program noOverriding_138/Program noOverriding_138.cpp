7cb20016fb0c745601a37ea3ea71a87e864a6ae4
NilaAyu1611 <nilaayucw@gmail.com>
2023-05-29 16:51:28 +0700

    buat class turunan dari baseClass -> derivedClass, tetapi disitu terjadi kesalahan/eror karena didalam method diclass baseClass itu ada sebuah speciefier final, yang menyebabkan method di turunannya itu eror

--- Program noOverriding_138/Program noOverriding_138.cpp
+++ Program noOverriding_138/Program noOverriding_138.cpp
@@ -7,3 +7,9 @@ public:
 		cout << "Hallo aya Function dari base class";
 	}
 };
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class";
+	}
+};
